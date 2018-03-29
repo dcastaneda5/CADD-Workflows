3e584aee1b4b764912c635cfdd01376dba127c0f
Ricardo Avila Escobedo <ravila11@login3.stampede2.tacc.utexas.edu>
2018-03-29 14:40:51 -0500

    Fix generated file names.

--- docking/split_mol2.py
+++ docking/split_mol2.py
@@ -2,12 +2,10 @@
 #  Script for splitting multiligand mol2 files #
 ################################################
 
-import sys
-import gzip
+# Usage: python split_mol2.py <mol2 ligand>
 
 def split_file(mol2_file):
     mol = ""
-    #with open(mol2_file, 'rt') as f:
     f = open(mol2_file, 'rt')
     for line in f:
         if line.startswith("@<TRIPOS>MOLECULE"):
@@ -17,11 +15,13 @@ def split_file(mol2_file):
             mol += line
         else:
             mol += line
+    f.close()
 
-file_name = sys.argv[1]
-root_name = file_name.split(".")[-2][1:]
-
-for count, contents in enumerate(split_file(file_name)):
-    outfile = open(root_name + "_" + str(count).zfill(5) + ".mol2", 'w')
-    #with open(root_name + "_" + str(count).zfill(5) + ".mol2", 'w') as outfile:
-    outfile.write(contents)
+if __name__ == "__main__":
+    import sys
+    file_name = sys.argv[1]
+    root_name = file_name.split(".")[-2]
+    for count, contents in enumerate(split_file(file_name)):
+        outfile = open(root_name + "_" + str(count).zfill(5) + ".mol2", 'w')
+        outfile.write(contents)
+        outfile.close()
