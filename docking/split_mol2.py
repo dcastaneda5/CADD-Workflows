1d74e7a34b74cf9baeadbaeb74203d277b2fcf7d
ravila4 <ravila@protonmail.com>
2018-04-02 15:21:46 -0600

    Refactor main function to easily import from another script. Support gzip.

--- docking/split_mol2.py
+++ docking/split_mol2.py
@@ -3,10 +3,12 @@
 ################################################
 
 # Usage: python split_mol2.py <mol2 ligand>
+import gzip
+import os
 
-def split_file(mol2_file):
+def generator(mol2_file):
     mol = ""
-    f = open(mol2_file, 'rt')
+    f = gzip.open(mol2_file, 'rt')
     for line in f:
         if line.startswith("@<TRIPOS>MOLECULE"):
             if mol != "":
@@ -17,11 +19,15 @@ def split_file(mol2_file):
             mol += line
     f.close()
 
-if __name__ == "__main__":
-    import sys
-    file_name = sys.argv[1]
-    root_name = file_name.split(".")[-2]
-    for count, contents in enumerate(split_file(file_name)):
+
+def split(mol2_file):
+    root_name = os.path.splitext(os.path.splitext(mol2_file)[0])[0]
+    for count, contents in enumerate(generator(mol2_file)):
         outfile = open(root_name + "_" + str(count).zfill(5) + ".mol2", 'w')
         outfile.write(contents)
         outfile.close()
+
+
+if __name__ == "__main__":
+    import sys
+    split(sys.argv[1])
