f49ef0d35359f97037c03e23cc4617423c6cc3d0
Ricardo Avila Escobedo <ravila@protonmail.com>
2018-04-03 10:36:50 -0500

    Fix target directories by adding slash.

--- docking/dock.py
+++ docking/dock.py
@@ -22,18 +22,18 @@ def dock(ligand):
     print("Running on ligand:", ligand)
     call([SMINA_PATH, "-r", receptor, "-l", ligand, "--autobox_ligand",
           crystal_ligand, "--num_modes", str(num_modes), "-o",
-          ligname + "_out.pdbqt", "--cpu", "2"])
+          ligname + "_out.pdbqt", "--cpu", "1"])
 
 
 if __name__ == "__main__":
     # User inputs
-    target_path = sys.argv[1]
+    target_path = sys.argv[1].rstrip("/")
     n_cpus = int(sys.argv[2])
     # Docking parameters
-    receptor = target_path + "receptor.pdbqt"
-    crystal_ligand = target_path + "crystal_ligand.mol2"
-    actives = glob.glob(target_path + "actives/*.pdbqt")
-    decoys = glob.glob(target_path + "decoys/*.pdbqt")
+    receptor = target_path + "/receptor.pdbqt"
+    crystal_ligand = target_path + "/crystal_ligand.mol2"
+    actives = glob.glob(target_path + "/actives/*.pdbqt")
+    decoys = glob.glob(target_path + "/decoys/*.pdbqt")
     ligands = actives + decoys
     num_modes = 12
     # Start process pool
